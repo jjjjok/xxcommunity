e924196f2a48df39dcf4eed5b4a0036910407621
jjjjok <2236702464@qq.com>
2021-04-29 23:32:14 +0800

    github登录

--- src/main/java/com/gugu/community/api/GithubApi.java
+++ src/main/java/com/gugu/community/api/GithubApi.java
@@ -2,6 +2,7 @@ package com.gugu.community.api;
 
 import com.alibaba.fastjson.JSON;
 import com.gugu.community.dto.AccessTokenDTO;
+import com.gugu.community.dto.GithubUser;
 import lombok.extern.slf4j.Slf4j;
 import okhttp3.*;
 import org.springframework.stereotype.Component;
@@ -24,6 +25,23 @@ public class GithubApi {
             return token;
         } catch (Exception e) {
             log.error("getAccessToken error,{}", accessTokenDTO, e);
+        }
+        return null;
+    }
+
+    public GithubUser getUser(String accessToken) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://api.github.com/user")
+                .header("Authorization", "token " + accessToken)
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            String string = response.body().string();
+            GithubUser githubUser = JSON.parseObject(string, GithubUser.class);
+            return githubUser;
+        } catch (Exception e) {
+            log.error("getUser error,{}", accessToken, e);
         }
         return null;
     }
--- src/main/java/com/gugu/community/controller/AuthorizeController.java
+++ src/main/java/com/gugu/community/controller/AuthorizeController.java
@@ -2,28 +2,50 @@ package com.gugu.community.controller;
 
 import com.gugu.community.api.GithubApi;
 import com.gugu.community.dto.AccessTokenDTO;
+import com.gugu.community.dto.GithubUser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class AuthorizeController {
     @Autowired
     private GithubApi githubApi;
 
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.client.secret}")
+    private String clientSecret;
+
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
     @GetMapping("callback")
-    public String callBack(@RequestParam(name = "code") String code,@RequestParam(name = "state") String state){
+    public String callBack(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request){
 
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("d592a6f654d23dfe620e");
-        accessTokenDTO.setClient_secret("4e4d3810828c2d2922c5183f77111fd121b725f8");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
+        String accessToken = githubApi.getAccessToken(accessTokenDTO);
+        GithubUser user = githubApi.getUser(accessToken);
 
-        githubApi.getAccessToken(accessTokenDTO);
-        return "index";
+        if (user != null && user.getId() != null) {
+            request.getSession().setAttribute("user",user);
+            return "redirect:/";
+        }else {
+            return "redirect:/";
+        }
 
     }
 }
